6b438fe435035c2c286de2d5d7abdbc91499d9b7
Zach Dennis <zach.dennis@gmail.com>
2015-08-16 17:26:51 -0400

    WIP Floats

--- block-flow.rb
+++ block-flow.rb
@@ -11,18 +11,62 @@ class Layout
     @y = offset_y
   end
 
+  def layout_floats(floats)
+    floats.each do |fbox|
+      if fbox.float == :left
+        # only allow the float to be as wide as its parent
+        if fbox.width > @box.width
+          fbox.width = @box.width
+        end
+
+        # if we cannot fit on this line, go to the next
+        if @x + fbox.width > @box.width
+          @x = 0
+          @y += 1
+        end
+
+        fbox.x = @x
+        fbox.y = @y
+
+        Layout.new(fbox, offset_x:@x, offset_y:@y).layout
+
+        @x += fbox.width
+      end
+    end
+  end
+
+  def starting_x_for_current_y
+    x = 0
+    @box.children.select { |cbox| cbox.display == :float }.each do |fbox|
+      next unless fbox.y >= @y && @y <= (fbox.y + fbox.height)
+      x = [fbox.x + fbox.width, x].max
+    end
+    x
+  end
+
   def layout
-    @box.children.each_with_index do |cbox, i|
-      previous_box = i > 0 ? @box.children[i - 1] : nil
+    floats = @box.children.select { |cbox| cbox.display == :float }
+    layout_floats(floats)
+
+    non_floats = @box.children.select { |cbox| cbox.display != :float }
+
+    non_floats.each_with_index do |cbox, i|
+      previous_box = i > 0 ? non_floats[i - 1] : nil
 
       if cbox.display == :block
-        @x = 0
-        @y += 1 if previous_box && previous_box.display == :inline
-        cbox.width = @box.width
+        @x = starting_x_for_current_y
+
+        if (previous_box && previous_box.display == :inline) || @x >= @box.width
+          @y += 1
+          @x = 0
+        end
+
+        cbox.width = (@box.width - @x)
         Layout.new(cbox, offset_x:@x, offset_y:@y).layout
         @box.height = (@box.height || 0) + cbox.height + (@box.content.to_s.length / @box.width.to_f).round
         cbox.x = @x
         cbox.y = @y
+
         @y += cbox.height
         @x = 0
       elsif cbox.display == :inline
@@ -33,6 +77,8 @@ class Layout
         @x = (@x + cbox.content.length) % @box.width
       end
     end
+
+    @box.children
   end
 end
 
@@ -45,9 +91,13 @@ class Box
     @children = children
   end
 
-  %w(width height display x y).each do |method|
+  %w(width height display x y float).each do |method|
     define_method(method){ style[method.to_sym] }
     define_method("#{method}="){ |value| style[method.to_sym] = value }
+  end
+
+  def width
+    style[:width] || 0
   end
 
   def height
@@ -75,6 +125,8 @@ end
 
 parent = Box.new(nil,
   children: [
+    Box.new("<"*2, style:{display: :float, float: :left, width:3}),
+    Box.new(">"*2, style:{display: :float, float: :left, width:3}),
     Box.new("A"*20, style: {display: :block}),
     Box.new("_"*3, style: {display: :inline}),
     Box.new("-"*3, style: {display: :inline}),
@@ -94,7 +146,7 @@ parent = Box.new(nil,
   ],
   style: {
     display: :block,
-    width: 80,
+    width: 10,
     height: nil
   }
 )
@@ -119,18 +171,29 @@ class TerminalRenderer
       if cbox.display == :block
         render_block cbox
       elsif cbox.display == :inline
-        if previous_box && previous_box.display == :block
-          needed_lines = cbox.y - (previous_box.y + previous_box.height) + 1
-          needed_lines.times{ $stdout.puts }
-          @y += needed_lines
-        end
+        # if previous_box && previous_box.display == :block
+          # needed_lines = cbox.y - (previous_box.y + previous_box.height) + 1
+          # needed_lines.times{ $stdout.puts }
+          # @y += needed_lines
+        # end
         render_inline cbox
+      elsif cbox.display == :float
+        render_float cbox
       end
     end
   end
 
+  def render_float(fbox)
+    @x = fbox.x
+    log "move to column #{fbox.x}"
+    move_to_column fbox.x
+    render_inline Box.new(fbox.content, style:{display: :inline, x:fbox.x, y:fbox.y})
+    render(fbox.children)
+  end
+
   def render_block(cbox)
-    @x = 0
+    @x = cbox.x
+
     log "move to column #{@x}"
     move_to_column @x
 
@@ -141,59 +204,39 @@ class TerminalRenderer
 
     if cbox.children.any?
       render(cbox.children)
-      @x = 0
+      @x = cbox.x
       $stdout.puts
       @y += 1
     end
 
-    count = 0
-    loop do
-      log "print #{cbox.content[count].inspect}    x:#{@x} y:#{@y} count:#{count}"
-      $stdout.print cbox.content[count]
-      count += 1
-      @x += 1
+    @x = cbox.x
 
-      if count >= cbox.content.length
-        log "breaking because count >= cbox.content.length (#{count} >= #{cbox.content.length})  x:#{@x} y:#{@y}"
-        break
-      end
-
-      if @x >= cbox.width
-        log "puts because x >= cbox.width (#{@x} >= #{cbox.width})    x:#{@x} y:#{@y}"
-        $stdout.puts
-        @x = 0
-        @y += 1
-      end
-
-      if @y > (cbox.height + cbox.y)
-        log "break because y > (cbox.height + cbox.y) (#{@y} > (#{cbox.height} + #{cbox.y})     x:#{@x} y:#{@y}"
-        break
-      end
-    end
-    @x = 0
+    render_inline(Box.new(cbox.content, style:{display: :inline, width: cbox.width, x:cbox.x, y:cbox.y}))
   end
 
   def render_inline(cbox)
     count = 0
     loop do
       if count >= cbox.content.length
-        # binding.pry
         log "breaking because count >= cbox.content.length (#{count} >= #{cbox.content.length})  x:#{@x} y:#{@y}"
         break
       end
 
-      if @x > 0 && (@x % cbox.width) == 0
+      if @x > cbox.x && cbox.width > 0 && (@x % (cbox.width + cbox.x)) == 0
         log "puts because inline content is at width (count % cbox.width) == 0   (#{count} % #{cbox.width}) == 0   x:#{@x} y:#{@y}"
         $stdout.puts
-        @x = 0
+        @x = cbox.x
         @y += 1
       end
 
       log "print #{cbox.content[count].inspect}    x:#{@x} y:#{@y} count:#{count}"
+      move_to_column @x
+
       $stdout.print cbox.content[count]
       count += 1
       @x += 1
     end
+
     log "Done inline: x:#{@x}  y:#{@y}"
   end
 
