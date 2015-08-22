e00706b97e511c93ca07722fb91f4c015d3724ad
Zach Dennis <zach.dennis@gmail.com>
2015-08-22 18:04:14 -0400

    Deleting Layout class. No longer necessary, everything is in RenderObject.

--- lib/terminal_layout.rb
+++ lib/terminal_layout.rb
@@ -232,63 +232,6 @@ module TerminalLayout
   class InlineRenderObject < RenderObject
   end
 
-  class Layout
-    def layout
-      laid_out_tree = []
-
-      @current_x = @x
-      @current_y = @y
-
-      @box.children.each do |cbox|
-        if cbox.display == :block
-          @current_x = starting_x_for_current_y
-          available_width = ending_x_for_current_y - @current_x
-
-          box2layout = Box.new(style: cbox.style.dup)
-          box2layout.width = @box.width
-          box2layout.children = cbox.children.dup
-          box2layout.x = @current_x
-          box2layout.y = @current_y
-          box2layout.children = Layout.new(box2layout).layout
-          box2layout.height = cbox.height
-
-          next if [nil, 0].include?(box2layout.width) || [nil, 0].include?(box2layout.height)
-
-          laid_out_tree.push box2layout
-          @current_x = 0
-          @current_y += [box2layout.height, 1].max
-        elsif cbox.display == :inline
-          @current_x = starting_x_for_current_y if @current_x == 0
-          available_width = ending_x_for_current_y - @current_x
-
-          content_i = 0
-          content = ""
-
-          loop do
-            chars_needed = available_width
-            partial_content = cbox.content[content_i...(content_i + chars_needed)]
-            chars_needed = partial_content.length
-            laid_out_tree << Box.new(content:partial_content, children:[], style: {display: :inline, x:@current_x, y: @current_y, width:chars_needed, height:1})
-
-            content_i += chars_needed
-
-            if @current_x + chars_needed > available_width
-              @current_y += 1
-              @current_x = starting_x_for_current_y
-              available_width = ending_x_for_current_y - @current_x
-            else
-              @current_x += chars_needed
-            end
-
-            break if content_i >= cbox.content.length
-          end
-        end
-      end
-
-      laid_out_tree
-    end
-  end
-
   class Box
     attr_accessor :style, :children, :content
 
